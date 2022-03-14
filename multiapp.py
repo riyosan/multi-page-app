641af08783fc8979e66014e361840d426e44345d
riyosan <64902143+riyosan@users.noreply.github.com>
2022-03-14 21:32:27 +0700

    Update multiapp.py

--- multiapp.py
+++ multiapp.py
@@ -41,7 +41,7 @@ class MultiApp:
     def run(self):
         # app = st.sidebar.radio(
         app = st.selectbox(
-            'Navigation',
+            'Selamat Datang',
             self.apps,
             format_func=lambda app: app['title'])
 
